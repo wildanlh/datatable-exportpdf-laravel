73285ca649c0a4118d766288e3ae89e566094ad1
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-07-02 21:34:23 +0700

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-10 ">
-    <h1 class="text-2xl font-bold">{{ __('Dashboard') }}</h1>
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-    @endif
+<div class="container mx-auto">
+    <div class="md:shadow-lg p-10 rounded mx-10">
+        <h1 class="text-2xl font-bold">{{ __('Dashboard') }}</h1>
+        @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+        @endif
 
-    {{ __('Kamu Sudah Login!') }} &#9989;
+        {{ __('Anda Telah Login!') }} &#9989;
+    </div>
 </div>
 @endsection
